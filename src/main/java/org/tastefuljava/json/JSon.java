908436b30697c1a50e4e7fe600f8cbc7b5482c30
maurice <maurice@perry.ch>
2017-10-29 11:00:07 +0100

    Added convenience methods in JSon

--- src/main/java/org/tastefuljava/json/JSon.java
+++ src/main/java/org/tastefuljava/json/JSon.java
@@ -1,6 +1,10 @@
 package org.tastefuljava.json;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringReader;
@@ -15,8 +19,18 @@ import org.tastefuljava.props.PropertyDef;
 import org.tastefuljava.util.InvocationLogger;
 
 public class JSon {
-    public static <T> T read(String json, Class<T> clazz) throws IOException {
-        return read(new StringReader(json), clazz);
+    public static <T> T read(File file, String encoding, Class<T> clazz)
+            throws IOException {
+        try (InputStream in = new FileInputStream(file)) {
+            return read(in, encoding, clazz);
+        }
+    }
+
+    public static <T> T read(InputStream in, String encoding, Class<T> clazz)
+            throws IOException {
+        try (Reader reader = new InputStreamReader(in, encoding)) {
+            return read(reader, clazz);
+        }
     }
 
     public static <T> T read(Reader in, Class<T> clazz)
@@ -24,12 +38,30 @@ public class JSon {
         return clazz.cast(readObject(in, new JSonBuilder(clazz)));
     }
 
-    public static Object read(String json) throws IOException {
-        return read(new StringReader(json));
+    public static <T> T read(String json, Class<T> clazz) throws IOException {
+        return read(new StringReader(json), clazz);
+    }
+
+    public static Object read(File file, String encoding)
+            throws IOException {
+        try (InputStream in = new FileInputStream(file)) {
+            return read(in, encoding);
+        }
+    }
+
+    public static Object read(InputStream in, String encoding)
+            throws IOException {
+        try (Reader reader = new InputStreamReader(in, encoding)) {
+            return read(reader);
+        }
     }
 
     public static Object read(Reader in) throws IOException {
         return readObject(in, new JSonGenericBuilder());
+    }
+
+    public static Object read(String json) throws IOException {
+        return read(new StringReader(json));
     }
 
     private static Object readObject(Reader in, AbstractJSonBuilder handler)
